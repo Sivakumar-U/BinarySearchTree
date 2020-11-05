40e398945ff136ef02247921a061a5a7be0366d0
Sivakumar-U <usivakumarreddy@gmail.com>
2020-11-05 18:56:00 +0530

    Added: Test case to check all are added with using size method in binary tree

--- src/test/java/com/blz/binarysearch/TestBinarySearch.java
+++ src/test/java/com/blz/binarysearch/TestBinarySearch.java
@@ -16,4 +16,25 @@ public class TestBinarySearch {
 		Assert.assertEquals(3, size);
 	}
 
+	// UC2
+	@Test
+	public void givenNumbers_AddedInBinarySearchTree() {
+		BinaryTree<Integer> binarySearch = new BinaryTree<>();
+		binarySearch.add(56);
+		binarySearch.add(30);
+		binarySearch.add(70);
+		binarySearch.add(60);
+		binarySearch.add(95);
+		binarySearch.add(65);
+		binarySearch.add(63);
+		binarySearch.add(67);
+		binarySearch.add(22);
+		binarySearch.add(40);
+		binarySearch.add(11);
+		binarySearch.add(3);
+		binarySearch.add(16);
+		int size = binarySearch.getSize();
+		Assert.assertEquals(13, size);
+	}
+
 }
